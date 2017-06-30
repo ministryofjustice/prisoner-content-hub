737d5349b88c217075e6442e3021522a59b8e1e4
StevenWilson <steve.wilson@digital.justice.gov.uk>
2017-06-30 13:42:04 +0100

    HUB-410 New Content Notification - Added notification markup to the main master layout tpl - Updated CSS to include styles for notifaction - Updated CSS to include notifaction CTA - Added close and star svgs - Add jquery to add interactive functionality

--- moj-fe/resources/views/layouts/master.blade.php
+++ moj-fe/resources/views/layouts/master.blade.php
@@ -24,14 +24,18 @@
 
     <body>
         @if ( Route::currentRouteName() == 'hub.sub' || Route::currentRouteName() == 'hub.landing' )
-            <div class="top-navigation hub-top-navigation">
-         <div class="row">
-              <div class="col-xs-12">
-                    @include('languageDropdown', ['path' => $path])
+            <div class="top-navigation hub-top-navigation ">
+                <div class="container">
+                <div class="row">
+                    <div class="col-xs-10 text-align-right">
+                        <a href="#" title="New content" id="new-content-trigger" class="cta cta-success notifaction-cta"><img src="img/star.svg" alt="New content icon" class="star-icon"> New Content</a>
+                    </div>
+                    <div class="col-xs-2">
+                        @include('languageDropdown', ['path' => $path])
+                    </div>
                 </div>
-              </div>
+                </div>
             </div>
-
         @else
         @yield('header')
         @endif
@@ -48,5 +52,67 @@
             &copy; <?php echo date("Y"); ?> {{ trans('footer.message') }}
         </footer>
         @endif
+
+        <div id="overlay" class="overlay"></div>
+
+        <section id="notifaction" class="notifaction">
+
+            <header>
+                <a href="#" class="close-notifaction" id="close-notifaction">
+                    <img src="img/close.svg" class="close-icon" alt="close button">
+                    Close
+                </a>
+                <h2>New Content</h2>
+                <p>12th Spetember 2017</p>
+            </header>
+
+            <section class="content books">
+                <h3>Books</h3>
+                <ul>
+                    <li><a href="#" title="Hello world">Nisl dictumst augue pulvinar libero.</a></li>
+                    <li><a href="#" title="Hello world">Quam senectus Praesent sit dignissim eu interdum</a></li>
+                    <li><a href="#" title="Hello world">Tempor auctor mauris urna ipsum aenean in</a></li>
+                    <li><a href="#" title="Hello world">Felis sociosqu praesent nascetur est habitasse</a></li>
+                    <li><a href="#" title="Hello world">Rutrum feugiat parturient.</a></li>
+                </ul>
+            </section>
+
+            <section class="content videos">
+                <h3>Videos</h3>
+                <h4>Timewise > Seeing How it is</h4>
+                <ul>
+                    <li><a href="#" title="Hello world">Nisl dictumst augue pulvinar libero.</a></li>
+                    <li><a href="#" title="Hello world">Quam senectus Praesent sit dignissim eu interdum</a></li>
+                </ul>
+                <h4>Timewise > Seeing How it is</h4>
+                <ul>
+                    <li><a href="#" title="Hello world">Nisl dictumst augue pulvinar libero.</a></li>
+                    <li><a href="#" title="Hello world">Quam senectus Praesent sit dignissim eu interdum</a></li>
+                </ul>
+                <h4>TED Talks > All the time</h4>
+                <ul>
+                    <li><a href="#" title="Hello world">Nisl dictumst augue pulvinar libero.</a></li>
+                    <li><a href="#" title="Hello world">Quam senectus Praesent sit dignissim eu interdum</a></li>
+                </ul>
+            </section>
+
+            <section class="content radio">
+                <h3>Radio</h3>
+                <h4>Bob and Gone</h4>
+                <ul>
+                    <li><a href="#" title="Hello world">Felis sociosqu praesent nascetur est habitasse</a></li>
+                    <li><a href="#" title="Hello world">Rutrum feugiat parturient.</a></li>
+                </ul>
+                <h4>Radio 7</h4>
+                <ul>
+                    <li><a href="#" title="Hello world">Nisl dictumst augue pulvinar libero.</a></li>
+                    <li><a href="#" title="Hello world">Quam senectus Praesent sit dignissim eu interdum</a></li>
+                </ul>
+            </section>
+
+        </section>
+
     </body>
 </html>
+
+
