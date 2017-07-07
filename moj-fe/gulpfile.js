bc6776194479e746668c6e0b3f3ebdcb42161f30
Rob Lazzurs <rob@lazzurs.org>
2017-07-07 19:35:23 +0100

    Adding jplayer to the required includes.
    Removed videoJS from a bunch of places it was not needed which should
    make a tiny difference to page load times without video.

--- moj-fe/gulpfile.js
+++ moj-fe/gulpfile.js
@@ -94,6 +94,18 @@ elixir( function (mix) {
         'node_modules/jplayer/dist/jplayer/jquery.jplayer.min.js',
         'public/js'
     ).copy(
+        'node_modules/jplayer/dist/jplayer/dist/skin/blue.monday/image/jplayer.blue.monday.jpg',
+        'public/image/'
+    ).copy(
+        'node_modules/jplayer/dist/jplayer/dist/skin/blue.monday/image/jplayer.blue.monday.seeking.gif',
+        'public/image/'
+    ).copy(
+        'node_modules/jplayer/dist/jplayer/dist/skin/blue.monday/image/jplayer.blue.monday.video.play.png',
+        'public/image/'
+    ).copy(
+        'node_modules/jplayer/dist/jplayer/dist/skin/blue.monday/css/jplayer.blue.monday.min.css',
+        'public/css/jplayer.css'
+    ).copy(
         'node_modules/jplayer/dist/jplayer/jquery.jplayer.swf',
         'public/js/res'
     );
