820d2996c4480d597df9f2dcef351319da3d8327
StevenWilson <steve.wilson@digital.justice.gov.uk>
2017-07-31 15:06:24 +0100

    HUB-436/SearchControllers - Added search Facade

---
+++ moj-fe/app/Facades/Search.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Search extends Facade {
+  protected static function getFacadeAccessor() {
+    return 'Search.repository';
+  }
+}
