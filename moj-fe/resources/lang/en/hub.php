2bde361d15437e56878280120563a61e0c10dd3c
StevenWilson <steve.wilson@digital.justice.gov.uk>
2017-08-22 15:15:33 +0100

    HUB-479/Added translations for 'Video', 'Radio', 'Books' and 'Added Today'

--- moj-fe/resources/lang/en/hub.php
+++ moj-fe/resources/lang/en/hub.php
@@ -3,6 +3,9 @@
 return [
   'title' => 'Digital Hub',
   'new_content' => 'New Content',
-  'search' => 'Search'
-
+  'search' => 'Search',
+  'video' => 'Video',
+  'radio' => 'Radio',
+  'books' => 'Books',
+  'added_today' => 'Added Today'
 ];
