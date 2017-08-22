fa9f5a31515b50da48a12e552eb4ef0688dcb22e
StevenWilson <steve.wilson@digital.justice.gov.uk>
2017-08-22 15:39:56 +0100

    HUB-479/Translated the search placeholder

--- moj-fe/resources/views/searchBox.blade.php
+++ moj-fe/resources/views/searchBox.blade.php
@@ -1,6 +1,6 @@
 <div class="searchBox form-group">
     <form method="GET" action="/search" accept-charset="UTF-8" search="contact_store" class="form">
-        <input type="text" placeholder="Search.." class="searchInput" name="q">
+        <input type="text" placeholder="{{ trans('hub.search') }}.." class="searchInput" name="q">
         <button type="submit" class="searchButton">
             <img src="/img/search.svg" class="searchIcon" alt="Search button">
         </button>
