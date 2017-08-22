9784777648900f0a9ca4d327441054e30ef01e4e
StevenWilson <steve.wilson@digital.justice.gov.uk>
2017-08-22 15:15:26 +0100

    HUB-479/Added translations for 'Video', 'Radio', 'Books' and 'Added Today'

--- moj-fe/resources/lang/cy/hub.php
+++ moj-fe/resources/lang/cy/hub.php
@@ -3,5 +3,9 @@
 return [
   'title' => 'Hwb Digidol',
   'new_content' => 'Cynnwys Newydd',
-  'search' => 'Chwilio'
+  'search' => 'Chwilio',
+  'video' => 'Fideo',
+  'radio' => 'Radio',
+  'books' => 'Llyfrau',
+  'added_today' => 'Wedi’i ychwanegu heddiw'
 ];
