b78ac0042b05ab6d67d2131d5a412bfbd8e3086e
StevenWilson <steve.wilson@digital.justice.gov.uk>
2017-08-23 10:16:39 +0100

    HUB-471 Added dynamic back link to Neontroids and Sudoku templates

--- moj-fe/resources/views/games/sudoku.blade.php
+++ moj-fe/resources/views/games/sudoku.blade.php
@@ -7,7 +7,7 @@
     <div class="top-navigation">
         <div class="row">
             <div class="col-xs-12">
-                <a href="/hub" class="back-to-hub">
+                <a href="{{ $backlink }}" class="back-to-hub">
                     <span class="icon icon-icon-hub" aria-hidden="true"></span>
                     <div class="back-to-the-hub-text">
                         {{ trans('navigation.title') }}
