ec6cd5ab43a26fe716c2c6ec395657320f8828fe
StevenWilson <steve.wilson@digital.justice.gov.uk>
2017-08-24 11:25:56 +0100

    HUB-472 Updated the video channel landing page template to use the new video player

--- moj-fe/resources/views/video/channelLandingPage.blade.php
+++ moj-fe/resources/views/video/channelLandingPage.blade.php
@@ -16,14 +16,53 @@
         </div>
 
         <div class="row">
-            <div class="col-md-5" id="playerColSize">
-                <video id="video_player" class="video-js vjs-default-skin vjs-big-play-centered" controls preload="auto" data-setup='{}' controls poster="{{ $data->thumbnail_url }}">
-                    <source src="{{ $data->video_url }}">
-                    <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
-                </video>
+            <div class="col-md-6" id="playerColSize">
+                <div id="jp_container_1" class="jp-video" role="application" aria-label="media player">
+                    <div class="jp-type-single">
+                        <div id="jquery_jplayer_1" class="jp-jplayer" style="">
+                            <img id="jp_poster_0" style="display: inline;" src="{{ $data->thumbnail_url }}">
+                            <video id="jp_video_0" preload="metadata" src="{{ $data->video_url }}" title="{{ $data->title }}" style="width: 0px; height: 0px;"></video>
+                        </div>
+                        <div class="jp-gui">
+                            <div class="jp-video-play" style="display: block;">
+                                <button class="jp-video-play-icon" role="button" tabindex="0">play</button>
+                            </div>
+                            <div class="jp-interface">
+                                <div class="jp-progress">
+                                    <div class="jp-seek-bar" style="width: 100%;">
+                                        <div class="jp-play-bar" style="width: 0%;"></div>
+                                    </div>
+                                </div>
+                                <div class="jp-current-time" role="timer" aria-label="time">00:00</div>
+
+                                <div class="jp-controls-holder">
+                                    <div class="jp-volume-controls">
+                                        <button class="jp-mute" role="button" tabindex="0">mute</button>
+                                        <button class="jp-volume-max" role="button" tabindex="0">max volume</button>
+                                        <div class="jp-volume-bar">
+                                            <div class="jp-volume-bar-value" style="width: 80%;"></div>
+                                        </div>
+                                    </div>
+                                    <div class="jp-controls">
+                                        <button class="jp-play" role="button" tabindex="0">play</button>
+                                        <button class="jp-stop" role="button" tabindex="0">stop</button>
+                                    </div>
+                                    <div class="jp-toggles">
+                                        <button class="jp-repeat" role="button" tabindex="0">repeat</button>
+                                        <button class="jp-full-screen" role="button" tabindex="0">full screen</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="jp-no-solution" style="display: none;">
+                            <span>Update Required</span>
+                            To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-7 video-details" id="textColSize">				
+            <div class="col-md-6 video-details" id="textColSize">
                 {!! $data->description !!}
             </div>
         </div>
@@ -70,5 +109,28 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#jquery_jplayer_1").jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    title: "{{ $data->title }}",
+                    m4v: "{{ $data->video_url }}",
+                    poster: "{{ $data->thumbnail_url }}"
+                });
+            },
+            cssSelectorAncestor: "#jp_container_1",
+            swfPath: "/js/res",
+            supplied: "m4v",
+            useStateClassSkin: true,
+            autoBlur: false,
+            smoothPlayBar: true,
+            keyEnabled: true,
+            remainingDuration: true,
+            toggleDuration: true
+        });
+    });
+</script>
 
 @endsection
