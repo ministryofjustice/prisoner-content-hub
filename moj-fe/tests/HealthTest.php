984c80688ea1745fe1494492121a417fb3108d12
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-02-27 12:17:18 +0000

    added two unit tests for reponse content and response code

--- moj-fe/tests/HealthTest.php
+++ moj-fe/tests/HealthTest.php
@@ -3,16 +3,31 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Http\Controllers\HealthController;
 
 class HealthTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    protected $timeStamp;
+    protected $healthController;
+
+    public function __construct()
     {
-        $this->assertTrue(true);
+        $this->healthController = new HealthController();
     }
+
+    public function setUp()
+    {
+        $this->timeStamp = time();
+    }
+
+    public function testResponseContentIsADateStamep()
+    {
+       $this->assertEquals($this->healthController->checkHealth()->getContent(), $this->timeStamp);
+    }
+
+    public function testResponseCodeIs200()
+    {
+        $this->assertEquals($this->healthController->checkHealth()->getStatusCode(), 200);
+    }
+
 }
