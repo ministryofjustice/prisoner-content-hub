29118ae705950252c99747879cec61384a27c2c4
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-02-27 10:10:10 +0000

    cleaned up HealthController

--- moj-fe/app/Http/Controllers/HealthController.php
+++ moj-fe/app/Http/Controllers/HealthController.php
@@ -2,23 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class HealthController extends Controller
 {
-  function checkHealth()
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    function checkHealth()
   {
-    // Disable page cache
-  //  $this->killSwitch->trigger();
-
     // Use plain response with timestamp
     $response = new Response();
     $response->setContent(time());
-
     return $response;
-
-    // return view('hub.hub', ['links' => $links]);
   }
 }
