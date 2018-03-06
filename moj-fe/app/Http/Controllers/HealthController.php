87821c571c044a8d38a28833d4fa35e87faa1ba1
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-03-06 15:37:27 +0000

    Ticket 56 - Setup Hub Dashboard - Separated the frontend and backend endpoints - Added in call to Drupal to check it is ok - Set response to return JSON data with a timestamp - If backend call fails a 500 page is displayed on the frontend

--- moj-fe/app/Http/Controllers/HealthController.php
+++ moj-fe/app/Http/Controllers/HealthController.php
@@ -3,17 +3,88 @@
 namespace App\Http\Controllers;
 
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Client;
 
 class HealthController extends Controller
 {
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    function checkHealth()
+
+    protected $client;
+
+    protected $response;
+
+    protected $cms;
+
+    protected $cmsHealth;
+
+    public function getResponse()
     {
-        // Use plain response with timestamp
-        $response = new Response();
-        $response->setContent(time());
-        return $response;
-     }
+        return $this->response;
+    }
+
+    public function getCmsHealth()
+    {
+        return $this->cmsHealth;
+    }
+
+    public function __construct()
+    {
+        $this->response = new Response();
+        $this->client = new Client(
+          [
+            'base_uri' => config('app.api_uri'),
+            'timeout' => config('app.timeout'),
+          ]
+        );
+    }
+
+    public function frontEndCheckHealthEndpoint()
+    {
+        $this->setHttpResponse(200);
+
+        return $this->response;
+    }
+
+    public function backEndCheckHealthEndpoint()
+    {
+        $this->cms = $this->checkCmsHealth();
+
+        if ($this->cms) {
+            $this->setHttpResponse($this->cms->getStatusCode());
+            return $this->response;
+        }
+        return response()->view('errors.500', [], 500);
+    }
+
+    public function checkCmsHealth()
+    {
+        try {
+            return $this->client->get('/api/health',[
+              'query' => [
+                '_format' => 'json'
+                ]
+              ]
+            );
+        } catch (RequestException $error) {
+            Log::error(Psr7\str($error->getRequest()));
+            if ($error->hasResponse()) {
+                Log::error(Psr7\str($error->getResponse()));
+            }
+        }
+    }
+
+    public function setHttpResponse($code = 200)
+    {
+        $this->response->setStatusCode($code);
+        $this->response->headers->set('Content-Type', 'application/json');
+        $this->response->setContent(
+          json_encode(
+            array(
+              'timestamp' => time()
+            )
+          )
+        );
+    }
 }
