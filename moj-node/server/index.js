86a7f94af22eb41c9716c5c62b02646d5f93cbb9
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-03-28 13:25:34 +0100

    Adding a new service

--- moj-node/server/index.js
+++ moj-node/server/index.js
@@ -2,12 +2,15 @@ const createApp = require('./app');
 const logger = require('../log');
 
 const createSomeService = require('./sevices/someService');
+const createOtherSomeService = require('./sevices/someOtherService');
 
 // pass in dependencies of service
 const someService = createSomeService();
+const someService = createOtherSomeService();
 
 const app = createApp({
     logger,
+    someService,
     someService
 });
 
