7e9c55038676c545f1e58cbb8312a3316c58ec83
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-04-13 14:02:10 +0100

    added menu service layer, and processer

--- moj-node/server/routes/index.js
+++ moj-node/server/routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
-module.exports = function ({logger, someService}) {
+module.exports = function ({logger, someService, someOtherService}) {
     const router = express.Router();
 
     router.get('/', (req, res) => {
-        logger.info('GET index');
+        // logger.info('GET index');
 
         const data = someService.getSomeData();
+        const dataOther = someOtherService.getSomeData();
 
-        res.render('index', {data})
+        res.render('index', {data, dataOther})
     });
 
     return router;
