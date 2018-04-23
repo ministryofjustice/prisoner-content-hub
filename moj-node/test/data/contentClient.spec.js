1a8b0773b2301965b56e2456ef23cc76a5376eee
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-04-23 10:44:15 +0100

    Cleaned up conten client test code styling

--- moj-node/test/data/contentClient.spec.js
+++ moj-node/test/data/contentClient.spec.js
@@ -1,12 +1,9 @@
 const contentClient = require('../../server/data/contentClient');
+const menuDataMock = require('../json/menuDataMock.json');
 
 describe('Test to ensure content client works', () => {
   it('Content client returns an array', () => {
-
-    const menuDataMock = require('../json/menuDataMock.json');
     const contentClientMock = sinon.mock(contentClient);
-    
     contentClientMock.expects('getMenu').returns(menuDataMock);
   });
 });
-  
