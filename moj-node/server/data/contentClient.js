aee0fa11e4ab7b37dddf39675f7b0facc219c0d0
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-05-01 09:38:10 +0100

    Replaced request wrapped in promise with superagent

--- moj-node/server/data/contentClient.js
+++ moj-node/server/data/contentClient.js
@@ -1,22 +1,18 @@
-const request = require('request');
+const logger = require('../../log.js');
+const request = require('superagent');
 
 module.exports = {
 
   getMenu: async () => {
-    const options = {
-      url: 'http://localhost:8182/api/menu_items/admin?_format=json',
-      headers: {
-        'User-Agent': 'request',
-      },
-    };
-    return new Promise(((resolve, reject) => {
-      request.get(options, (err, resp, body) => {
-        if (err) {
-          reject(err);
-        }
-        return resolve(JSON.parse(body));
-      });
-    }));
+    try {
+      const result = await request
+        .get('http://localhost:8182/api/menu_items/admin')
+        .set('Content-Type', 'application/json')
+        .query({ _format: 'json' });
+      return result.body;
+    } catch (error) {
+      logger.error('Error: ', error.stack);
+      throw error;
+    }
   },
-
 };
