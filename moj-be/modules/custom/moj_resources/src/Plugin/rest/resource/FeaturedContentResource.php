7834f02a260f0fd44366a2b27f876d92cbeafbd6
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-06-15 17:22:39 +0100

    API resource class and config

--- moj-be/modules/custom/moj_resources/src/Plugin/rest/resource/FeaturedContentResource.php
+++ moj-be/modules/custom/moj_resources/src/Plugin/rest/resource/FeaturedContentResource.php
@@ -1,36 +1,78 @@
 <?php
 
+/**
+ * @file
+ * Contains Drupal\\moj_resources\Plugin\rest\resource\FeaturedContentResource.
+ */
+
 namespace Drupal\moj_resources\Plugin\rest\resource;
 
-use Drupal\rest\Plugin\ResourceBase;
+use Psr\Log\LoggerInterface;
 use Drupal\rest\ResourceResponse;
-use Drupal\moj_resources\Controller\FeaturedContentApiController;
-
+use Drupal\rest\Plugin\ResourceBase;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\moj_resources\FeaturedContentApiClass;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Provides a Featured Content Resource
  *
  * @RestResource(
  *   id = "featured_content_resource",
- *   label = @Translation("Fatured Content Resource"),
+ *   label = @Translation("Featured content resource"),
  *   uri_paths = {
- *     "canonical" = "/api/node/featured"
+ *     "canonical" = "/api/node/featured/{lang}"
  *   }
  * )
  */
 
-class FeaturedContentResource extends ResourceBase {
+class FeaturedContentResource extends ResourceBase 
+{
+    protected $featuredContentApiController;
+
+    protected $currentRequest;
+
+    public function __construct(
+        array $configuration,
+        $plugin_id,
+        $plugin_definition,
+        array $serializer_formats,
+        LoggerInterface $logger,
+        FeaturedContentApiClass $FeaturedContentApiClass,
+        Request $currentRequest
+    ) {        
+        $this->featuredContentApiClass = $FeaturedContentApiClass;
+        $this->currentRequest = $currentRequest;
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    }
   
-  /**
-     * Responds to entity GET requests.
-     * @return \Drupal\rest\ResourceResponse
-     */
+    public static function create(
+        ContainerInterface $container,
+        array $configuration, 
+        $plugin_id, 
+        $plugin_definition
+    ) {
+        return new static(
+            $configuration,
+            $plugin_id,
+            $plugin_definition,
+            $container->getParameter('serializer.formats'),
+            $container->get('logger.factory')->get('rest'),
+            $container->get('moj_resources.featured_content_api_class'),
+            $container->get('request_stack')->getCurrentRequest()
+        );
+    }   
+
     public function get() 
     {
-      $FeaturedContentApiController = new FeaturedContentApiController();
-      $result = $FeaturedContentApiController->FeaturedContentApiEndpoint();
-      $response = new ResourceResponse($result);
-      return $response->addCacheableDependency($result);
+        $lang = $this->currentRequest->get('lang');
+        $featured = $this->featuredContentApiClass->FeaturedContentApiEndpoint($lang);
+        if (!empty($featured)) {
+            return new ResourceResponse($featured);
+        }
+        throw new NotFoundHttpException(t('No featured content found'));
     }
 }
 
