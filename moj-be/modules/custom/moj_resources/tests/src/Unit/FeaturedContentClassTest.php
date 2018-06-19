9838716eafd0e0a6e6aa297e855d3f1b6be345d2
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-06-19 10:20:49 +0100

    Way to mocking setup.. will need to switch to a KernalUnitTest

--- moj-be/modules/custom/moj_resources/tests/src/Unit/FeaturedContentClassTest.php
+++ moj-be/modules/custom/moj_resources/tests/src/Unit/FeaturedContentClassTest.php
@@ -3,53 +3,99 @@
 namespace Drupal\Tests\moj_resources\Unit;
 
 use Drupal\Tests\UnitTestCase;
+use Drupal\Tests\Core\Form\FormTestBase;
 use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\moj_resources\_FeaturedContentApiClass;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\moj_resources\Controller\FeaturedContentApiController;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
 
 /**
  * Featured contnet API Unit tests
  *
- * @group moj_resources
+ * @group unit_moj_resources
  */
 
-class NewContentTest extends UnitTestCase
+class NewContentTest extends FormTestBase
 {
     /**
-     * @var \Drupal\moj_resources\Controller\FeaturedContentApiController
+     * @var \Drupal\moj_resources\Controller\_FeaturedContentApiController
      */
-    public $featuredContentApiController;
+    public $featuredContentApiClass;
 
-    public $mockEntityTypeManager;
+    public $entityManager;
 
-    public $mockEntityQuery;
+    public $entityQuery;
+
+    public $entityQueryFactory;
+
+    public $node;
+
+    public $nodeStorage;
+
+    public $node_title;
 
     public function setUp()
     {
-        $this->mockEntityTypeManager = $this->getMock('Drupal\Core\Entity\EntityTypeManagerInterface');
+        parent::setUp(); 
 
-        $this->mockEntityQuery = $this->getMockBuilder('Drupal\Core\Entity\Query\QueryFactory')
+        /* Entity Manager */
+
+        $this->node_title = $this->getRandomGenerator()->word(10);
+
+        $this->node = $this->getMockBuilder('Drupal\node\Entity\Node')
             ->disableOriginalConstructor()
             ->getMock();
         
-        $this->mockEntityQuery->expects($this->any())
-            ->method('get')
+        $this->node->expects($this->any())
+            ->method('getTitle')
+            ->will($this->returnValue($this->node_title));
+        
+        // print_r($this->node);
+        
+        $this->nodeStorage = $this->getMockBuilder('Drupal\node\NodeStorage')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $this->nodeStorage->expects($this->any())
+            ->method('loadMultiple')
+            ->will($this->returnValue(array(4727 => $this->node)));
+        
+        $this->entityManager = $this->getMockBuilder('Drupal\Core\Entity\EntityManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $this->entityManager->expects($this->any())
+            ->method('getStorage')
+            ->with('node')
+            ->willReturn($this->nodeStorage); 
+        
+        /* Query Factory */
+
+        $this->entityQuery = $this->getMockBuilder('Drupal\Core\Entity\Query\QueryInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->entityQueryFactory = $this->getMockBuilder('Drupal\Core\Entity\Query\QueryFactory')
+            ->disableOriginalConstructor()
+            ->setMethods(array('get', 'condition', 'sort', 'range', 'execute'))
+            ->getMock();
+        
+        $this->entityQueryFactory->expects($this->at(5)) // on index 5 of call 6
+            ->method('execute')
             ->will($this->returnValue(array(4727 => 4727)));
+        
+        $this->entityQueryFactory->expects($this->any())
+            ->method($this->anything())
+            ->will($this->returnSelf());
+        
+        $this->featuredContentApiClass = new _FeaturedContentApiClass($this->entityManager, $this->entityQueryFactory);
     }
 
-    public function testMockEntityManager() 
+    public function testGetFeaturedContentReturnsNodeId()
     {
-        $this->assertTrue($this->mockEntityTypeManager instanceof EntityTypeManagerInterface);
-    }
 
-    public function testMockEntityQuery() 
-    {
-        $this->assertTrue($this->mockEntityQuery instanceof QueryFactory);
-    }
-
-    public function testMockEntityQueryReturnsAnArray() 
-    {
-        $this->assertInternalType("array", $this->mockEntityQuery->get('node'));
-
+        $nids = $this->featuredContentApiClass->getNids();
+        
+        $this->assertEquals(array(4727 => 4727), $nids);
     }
 }
