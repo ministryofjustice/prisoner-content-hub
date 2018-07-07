2c403342c9120ac39f08e2a5e731279659b361dd
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-07-07 11:13:18 +0100

    Added demo data service

--- moj-node/server/routes/index.js
+++ moj-node/server/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-module.exports = function Index({ logger, someService }) {
+module.exports = function Index({ logger, demoDataService }) {
   const router = express.Router();
 
   router.get('/', (req, res) => {
     logger.info('GET index');
 
-    const data = someService.getSomeData();
+    const data = demoDataService.getInspirationData();
 
     res.render('pages/index', { data });
   });
---
+++ moj-node/server/services/demoDataService.js
@@ -0,0 +1,59 @@
+module.exports = function createDemoDataService() {
+  function getInspirationData() {
+    return {
+      number: 4,
+      category_title: 'Inspiration',
+      category_link: '#',
+      items: {
+        item1: {
+          title: 'Hello world',
+          description: 'Here is a short description',
+          size: 'small',
+          grid: 'govuk-grid-column-one-quarter',
+          type: 'inspiration',
+          icon: 'icon-game',
+          linkclass: 'icon-link',
+          linktext: 'Play',
+          graphic: '/images/content-image.jpg'
+        },
+        item2: {
+          title: 'Hello People',
+          description: 'Here is a shorter description',
+          size: 'small',
+          grid: 'govuk-grid-column-one-quarter',
+          type: 'inspiration',
+          icon: 'icon-document',
+          linkclass: 'icon-play',
+          linktext: 'Listen',
+          graphic: '/images/content-image.jpg'
+        },
+        item3: {
+          title: 'Hello Cats',
+          description: 'Here is a Long description',
+          size: 'small',
+          grid: 'govuk-grid-column-one-quarter',
+          type: 'inspiration',
+          icon: 'icon-movie',
+          linkclass: 'icon-link',
+          linktext: 'Read',
+          graphic: '/images/content-image.jpg'
+        },
+        item4: {
+          title: 'Hello Dogs',
+          description: 'Here is a description',
+          size: 'small',
+          grid: 'govuk-grid-column-one-quarter',
+          type: 'inspiration',
+          icon: 'icon-music',
+          linkclass: 'icon-play',
+          linktext: 'Play',
+          graphic: '/images/content-image.jpg'
+        }
+      }
+    };
+  }
+
+  return {
+    getInspirationData,
+  };
+};
