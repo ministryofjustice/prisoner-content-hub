c729b8ca3eaf6ca0a02755b5f294812abdcbffb3
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-08-12 22:30:42 +0100

    Add setting.local.php optional include into main settings.php

--- moj-be/sites/default/settings.php
+++ moj-be/sites/default/settings.php
@@ -72,9 +72,18 @@ $databases['default']['default'] = array (
 );
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = 'sites/default/files/config/sync';
-// $settings['trusted_host_patterns'] = array(
-// '^localhost$',
-// );
 
-// $settings['reverse_proxy'] = TRUE;
-// $settings['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
+/**
+ * Load local development override configuration, if available.
+ *
+ * Use settings.local.php to override variables on secondary (staging,
+ * development, etc) installations of this site. Typically used to disable
+ * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
+ * other things that should not happen on development and testing sites.
+ *
+ * Keep this code block at the end of this file to take full effect.
+ */
+
+ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
