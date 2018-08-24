db2e248b50b0f5d295a254a899bc305ab8088e1c
StevenWilson <steve.wilson@digital.justice.gov.uk>
2018-08-24 09:29:27 +0100

    Reactor promoted content to match new app architecture

---
+++ moj-node/server/repositories/hubPromotedContent.js
@@ -0,0 +1,18 @@
+
+const { parseResponse } = require('../utils/index');
+const config = require('../config');
+
+function hubPromotedContentRepository(httpClient) {
+
+  async function hubPromotedContent(opts = { query: {} }) {
+    const response = await httpClient.get(`${config.api.hubContent}/promoted`, opts.query);
+    if(response.message) return null
+    return parseResponse(response);
+  }
+
+  return {
+    hubPromotedContent
+  }
+}
+
+module.exports = hubPromotedContentRepository;
--- moj-node/server/services/hubPromotedContent.js
+++ moj-node/server/services/hubPromotedContent.js
@@ -1,20 +1,12 @@
-const { HubContentClient, HubFeaturedContentResponse } = require('../clients/hubContent');
-
-module.exports = function createHubPromotedContentService() {
-
-  async function hubContentPromotedFor(httpClient, opts = { query: {}}) {
-    const response = await httpClient.get("/promoted", opts.query);
-    if(response.message) return null
-    return HubFeaturedContentResponse.parse(response);
+module.exports = function createHubPromotedContentService(repository) {
+  async function hubPromotedContent() {
+    try {
+      return repository.hubPromotedContent();
+    } catch (ex) {
+      return null;
+    }
   }
-  
-  async function hubPromotedContent(httpClient) {
-    const client = new HubContentClient(httpClient);
-    return hubContentPromotedFor(client) 
-  }
-
   return {
-    hubContentPromotedFor,
-    hubPromotedContent
-  }
-}
+    hubPromotedContent,
+  };
+};
