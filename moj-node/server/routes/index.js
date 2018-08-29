646e2b9b0203830e971ebbd9c489d977ed439aa6
David Thomas <davidthomas1@digital.justice.gov.uk>
2018-08-29 12:07:02 +0100

    Add index route test coverage

--- moj-node/server/routes/index.js
+++ moj-node/server/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 module.exports = function Index({
   logger,
-  demoDataService,
   hubFeaturedContentService,
   hubPromotedContentService,
 }) {
@@ -11,9 +10,6 @@ module.exports = function Index({
   router.get('/', async (req, res) => {
     try {
       logger.info('GET index');
-      const gamedata = demoDataService.getGamesData();
-      const newseventsData = demoDataService.getNewsEventsData();
-
       const featuredContent = await hubFeaturedContentService.hubFeaturedContent();
       const [promotionalContent] = await hubPromotedContentService.hubPromotedContent();
 
@@ -26,92 +22,11 @@ module.exports = function Index({
       res.render('pages/index', {
         ...featuredContent,
         promotionalContent,
-        gamedata,
-        newseventsData,
         config,
       });
     } catch (exception) {
       logger.error(exception);
       res.sendStatus(500);
-    }
-  });
-
-  // router.get('/content/:contentName', (req, res) => {
-  //   try {
-  //     const contentPath = `../data/content/${req.params.contentName}`;
-  //     const content = require(contentPath); // eslint-disable-line
-  //     res.render('pages/flat-content', content);
-  //   } catch (exp) {
-  //     res.status(404);
-  //     res.send('Page not found');
-  //   }
-  // });
-
-  router.get('/landing', (req, res) => {
-    try {
-      const landingpagesubmenudata = demoDataService.getLandingPageSubMenuData();
-      const youmightlikedata = demoDataService.getYouMightLike();
-      const config = {
-        content: true,
-        header: true,
-        postscript: false,
-      };
-      const data = {
-        headerClass: 'healthy-mind-body',
-      };
-      res.render('pages/landing', {
-        data,
-        config,
-        landingpagesubmenudata,
-        youmightlikedata,
-      });
-    } catch (exp) {
-      res.status(404);
-      res.send('Page not found');
-    }
-  });
-
-  router.get('/video', (req, res) => {
-    try {
-      const watchnextdata = demoDataService.getWatchNextData();
-      const youmightlikedata = demoDataService.getYouMightLike();
-      const youmightlikesmalldata = demoDataService.getYouMightLikeSmallData();
-      const config = {
-        content: true,
-        header: false,
-        postscript: false,
-      };
-      res.render('pages/video', {
-        config,
-        watchnextdata,
-        youmightlikedata,
-        youmightlikesmalldata,
-      });
-    } catch (exp) {
-      res.status(404);
-      res.send('Page not found');
-    }
-  });
-
-  router.get('/audio', (req, res) => {
-    try {
-      const listennextdata = demoDataService.getListenNextData();
-      const youmightlikedata = demoDataService.getYouMightLike();
-      const youmightlikesmalldata = demoDataService.getYouMightLikeSmallData();
-      const config = {
-        content: true,
-        header: false,
-        postscript: false,
-      };
-      res.render('pages/audio', {
-        config,
-        listennextdata,
-        youmightlikedata,
-        youmightlikesmalldata,
-      });
-    } catch (exp) {
-      res.status(404);
-      res.send('Page not found');
     }
   });
 
---
+++ moj-node/test/routes/index.spec.js
@@ -0,0 +1,111 @@
+const request = require('supertest');
+const cheerio = require('cheerio');
+
+const createIndexRouter = require('../../server/routes/index');
+const { setupBasicApp, logger } = require('../test-helpers');
+
+
+describe('GET /', () => {
+  const featuredItem = {
+    id: 1,
+    title: 'foo title',
+    contentType: 'foo',
+    description: {
+      sanitized: 'foo summary',
+    },
+    image: {
+      alt: 'Foo image alt text',
+      url: 'image.url.com',
+    },
+    duration: '40:00',
+  };
+
+  const hubFeaturedContentService = {
+    hubFeaturedContent: sinon.stub().returns({
+      newsAndEvents: [],
+      games: [],
+      radioShowsAndPodcasts: [{ ...featuredItem, title: 'Foo radio show' }],
+      healthyMindAndBody: [{ ...featuredItem, id: 2 }],
+      inspiration: [{ ...featuredItem, id: 3 }],
+      scienceAndNature: [{ ...featuredItem, id: 4 }],
+      artAndCulture: [{ ...featuredItem, id: 5 }],
+      history: [{ ...featuredItem, id: 6 }],
+    }),
+  };
+
+  const hubPromotedContentService = {
+    hubPromotedContent: sinon.stub().returns([{
+      ...featuredItem,
+      title: 'foo promoted content',
+      description: {
+        sanitized: 'foo promoted summary',
+      },
+    }]),
+  };
+
+  it('renders promoted content', () => {
+    const router = createIndexRouter({
+      logger,
+      hubFeaturedContentService,
+      hubPromotedContentService,
+    });
+    const app = setupBasicApp();
+
+    app.use(router);
+
+    return request(app)
+      .get('/')
+      .expect(200)
+      .then((response) => {
+        const $ = cheerio.load(response.text);
+
+        expect($('[data-promoted-item-text]').text()).to.include('foo promoted content', 'Correct title rendered');
+        expect($('[data-promoted-item-text]').text()).to.include('foo promoted summary', 'Correct description rendered');
+        expect($('[data-promoted-item-background]').attr('style')).to.include('image.url.com', 'Correct image rendered');
+      });
+  });
+
+  it('renders featured content', () => {
+    const router = createIndexRouter({
+      logger,
+      hubFeaturedContentService,
+      hubPromotedContentService,
+    });
+    const app = setupBasicApp();
+
+    app.use(router);
+
+    return request(app)
+      .get('/')
+      .expect(200)
+      .then((response) => {
+        const $ = cheerio.load(response.text);
+        const radioItemSelector = '[data-featured-item-id="featured-content-1"]';
+
+        expect($('[data-featured-item-id]').length).to.equal(6, '6 featured items to be renderd');
+        expect($(radioItemSelector).text()).to.include('Foo radio show', 'Correct title rendered');
+        expect($(radioItemSelector).text()).to.include('foo summary', 'Correct description rendered');
+        expect($(`${radioItemSelector} [data-featured-item-background]`).attr('style')).to.include('image.url.com', 'Correct image rendered');
+        expect($(`${radioItemSelector} [data-featured-item-duration]`).text()).to.include('40:00', 'Correct duration rendered');
+      });
+  });
+
+  it('render a 500 when there is an error', () => {
+    const hubFeaturedContentServiceFailure = {
+      hubFeaturedContent: sinon.stub().throws('boom'),
+    };
+
+    const router = createIndexRouter({
+      logger,
+      hubFeaturedContentService: hubFeaturedContentServiceFailure,
+      hubPromotedContentService,
+    });
+    const app = setupBasicApp();
+
+    app.use(router);
+
+    return request(app)
+      .get('/')
+      .expect(500);
+  });
+});
