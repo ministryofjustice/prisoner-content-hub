49f190c27a149c322fb42a065980036f17e8db4c
Steven Bapaga <sbapaga@gmail.com>
2018-09-04 14:50:22 +0100

    Add date to page header

--- moj-node/assets/javascript/hub.js
+++ moj-node/assets/javascript/hub.js
@@ -1,7 +1,34 @@
 /* eslint-env browser */
 
 const navMenu = document.getElementById('nav-menu');
+const todaysDate = document.querySelector('[data-target-id="todays-date"]');
+const todaysTime = document.querySelector('[data-target-id="todays-time"]');
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+// Navigation menu
 if (navMenu) {
   document.body.addEventListener('click', (event) => {
     // handle navigation hide
@@ -32,7 +59,37 @@ if (navMenu) {
   });
 }
 
+// update the time
+if(todaysTime) {
+  setInterval(() => {
+    const date = new Date();
+    const currentTime = todaysTime.textContent.trim();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const nextTime =  formateTime(hours) + ":" + formateTime(minutes);
+    if (currentTime !== nextTime) {
+      todaysTime.textContent = nextTime;
+    }
+  }, 1000);
+}
+
+// update the date
+if(todaysDate) {
+  setInterval(() => {
+    const date = new Date();
+    const currentDate = todaysDate.textContent.trim();
+    const nextDate = days[date.getDay()] + " " + date.getDate() + " " + months[date.getMonth()];
+    if (currentDate !== nextDate) {
+      todaysDate.textContent = nextDate
+    }
+  }, 1000);
+}
+
 
 function matchesSelectorAll(target, selector) {
   return target.matches(selector) || target.matches(`${selector} *`);
+}
+
+function formateTime(time) {
+  return (time < 10) ? "0" + time : time
 }
