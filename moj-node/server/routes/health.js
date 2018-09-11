02400e6d785f28be147841e9094f8b3aa691a102
Steven Bapaga <sbapaga@gmail.com>
2018-09-11 13:32:09 +0100

    Add build info on circle build
    - Ensure that health endpoind doesn't crash application
    - Update docker build instructions

--- moj-node/server/routes/health.js
+++ moj-node/server/routes/health.js
@@ -3,8 +3,12 @@ const express = require('express');
 module.exports = function Health({ appInfo }) {
   const router = express.Router();
 
-  router.get('/', (req, res) => {
-    res.json({ status: 'OK', ...appInfo.getBuildInfo() });
+  router.get('/', (req, res, next) => {
+    try {
+      res.json({ status: 'OK', ...appInfo.getBuildInfo() });
+    } catch (exp) {
+      next(exp);
+    }
   });
 
   return router;
