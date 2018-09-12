d0b610126721bb3faf7e0c3f6d7edd51337e1f23
Steven Wilson <steve.wilson@digital.justice.gov.uk>
2018-09-12 11:34:09 +0100

    Add e2e tests for radio shows and podcasts on the home page (#134)

--- moj-node/test/e2e/homepage-test.spec.js
+++ moj-node/test/e2e/homepage-test.spec.js
@@ -7,7 +7,6 @@ describe('Home page', () => {
   before(async () => {
     browser = await puppeteer.launch();
     page = await browser.newPage();
-    await page.goto('http://localhost:3000');
   });
 
   after(async () => {
@@ -16,7 +15,9 @@ describe('Home page', () => {
 
   describe('News and event featured items', () => {
     let container;
+
     before(async () => {
+      await page.goto('http://localhost:3000');
       container = await page.$('[data-featured-section-id="news-events"]');
     });
 
@@ -30,6 +31,46 @@ describe('Home page', () => {
       const featuredItemsCount = await container.$$eval('[data-featured-item-id]', node => node.length);
 
       expect(featuredItemsCount).to.equal(4);
+    });
+
+    it('navigates to the correct content for a featured event', async () => {
+      const featuredEvent = await container.$('[data-featured-item-id]:first-child');
+
+
+      const featuredId = await featuredEvent.$eval('[data-featured-id]', node => node.getAttribute('data-featured-id'));
+      const featuredTitle = await featuredEvent.$eval('[data-featured-title]', node => node.getAttribute('data-featured-title'));
+
+      const [response] = await Promise.all([
+        page.waitForNavigation(),
+        featuredEvent.click(),
+      ]);
+
+      const responseText = await response.text();
+
+      expect(response.url()).to.contain(`/content/${featuredId}`);
+      expect(responseText).to.contain(featuredTitle);
+    });
+  });
+
+  describe('Radio shows and podcasts', () => {
+    let container;
+
+    before(async () => {
+      await page.goto('http://localhost:3000');
+      container = await page.$('[data-featured-section-id="radio-podcasts"]');
+    });
+
+
+    it('renders the correct section title', async () => {
+      const sectionTitle = await container.$eval('[data-featured-section-title]', node => node.textContent);
+
+      expect(sectionTitle).to.equal('Radio shows and podcasts');
+    });
+
+    it('renders the correct number of news and event featured items', async () => {
+      const featuredItemsCount = await container.$$eval('[data-featured-item-id]', node => node.length);
+
+      expect(featuredItemsCount).to.equal(8);
     });
 
     it('navigates to the correct content for a featured event', async () => {
