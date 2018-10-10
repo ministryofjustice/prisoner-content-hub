8b562cb1fb468d2a7eeb9be36faf7c8dd3275c52
Jon Nield <7629656+Tiki-God@users.noreply.github.com>
2018-10-10 14:48:12 +0100

    Add in some colour to hilight stages

--- berwyn-deployment-scripts/image_transfer.sh
+++ berwyn-deployment-scripts/image_transfer.sh
@@ -2,6 +2,11 @@
 # Pull an image from docker hub, save it, transfer it to Berwyn and import to the local docker image repo
 set -e -u -o pipefail
 
+# Some terminal colour
+GREEN="[1;32m"
+DEF="[39m"
+STAR="${GREEN}*${DEF}"
+
 main() {
   case ${1:-} in
   be)
@@ -33,21 +38,21 @@ transfer_image() {
   local filename="${image//:/_}"
   local filename="${filename//\//_}.tar"
 
-  echo " [*] Pulling ${image}"
+  echo " [${STAR}] Pulling ${image}"
   sudo docker pull "${image}"
 
-  echo " [*] Saving image ${image} as ${filename}"
+  echo " [${STAR}] Saving image ${image} as ${filename}"
   sudo docker save -o "/mnt/${filename}" "${image}"
   sudo chmod 0644 "/mnt/${filename}"
 
   # We enable SSH compression in this transfer as the Berwyn connection isn't the fastest
-  echo " [*] Transfering image to prod, you'll be prompted for the root password"
+  echo " [${STAR}] Transfering image to prod, you'll be prompted for the root password"
   scp -q -C -o PasswordAuthentication=yes "/mnt/${filename}" root@berwyn.mycloudgateway.co.uk:~
 
-  echo " [*] Importing image to Berwyn local docker repo, you'll be prompted for the root password"
+  echo " [${STAR}] Importing image to Berwyn local docker repo, you'll be prompted for the root password"
   ssh -q -o PasswordAuthentication=yes root@berwyn.mycloudgateway.co.uk "docker load -i ${filename} && rm ${filename}"
 
-  echo " [*] Removing local copies"
+  echo " [${STAR}] Removing local copies"
   sudo rm "/mnt/${filename}"
   sudo docker rmi "${image}"
 }
