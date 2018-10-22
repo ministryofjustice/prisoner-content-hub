5be45d90ebfebb0c6f51fed934f3538482bedebd
Jon Nield <7629656+Tiki-God@users.noreply.github.com>
2018-10-22 12:55:49 +0100

    New/updated deployment scripts

---
+++ berwyn-deployment-scripts/3. Assets Transfer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail
+
+# Asset transfer from stagingn to prod
+# Requires the public key from staging placing in the authorized keys on prod
+
+ssh -R 50000:berwyn.mycloudgateway.co.uk:22 digital-hub-stage.hmpps.dsd.io 'rsync -n -riP --ignore-existing -e "ssh -p50000 -o PasswordAuthentication=yes -oStrictHostKeyChecking=no" /content/moj_dhub_prod001_app/usr/share/nginx/html/moj_be/sites/default/files/ root@localhost:/content/moj_dhub_prod001_app/usr/share/nginx/html/moj_be/sites/default/files/'
