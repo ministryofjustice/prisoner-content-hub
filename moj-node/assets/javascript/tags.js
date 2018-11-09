a9837fb84c50d9fdce216ff5dbb0d185aeb8b58c
Steven Bapaga <sbapaga@gmail.com>
2018-11-09 09:29:11 +0000

    Fix duplication of content bug (#189)
    * Fix duplication of content bug
    * Remove rouge arrow function
    * Arrow functions are not supported by I.E

--- moj-node/assets/javascript/tags.js
+++ moj-node/assets/javascript/tags.js
@@ -1,5 +1,4 @@
-
-(function() {
+;(function() {
   var template = document.getElementById('template').innerHTML;
   var relatedContents = document.getElementById('related-contents');
   var contentId = relatedContents.getAttribute('data-content-id');
@@ -7,23 +6,29 @@
 
   var url = "/tags/related-content/" + contentId;
   var customTags = [ '<%', '%>' ];
-  var currentOffset = 9;
+  var currentOffset = 0;
+  var paginateOffset = 9
 
 
   // Mustache stuff;
   Mustache.parse(template);
   Mustache.tags = customTags;
 
+  // Load the first few to;
+  addContent(currentOffset);
 
-  function getContent(query, cb) {
+  // Load the rest as the user scrolls
+  window.addEventListener('scroll', handleScroll);
+
+  function getContent(query, callback) {
     var request = new XMLHttpRequest();
 
     request.addEventListener('load', function(response) {
-      cb(undefined, response.target);
+      callback(undefined, response.target);
     });
 
     request.addEventListener('error', function() {
-      cb(true, undefined);
+      callback(true, undefined);
     });
 
     request.addEventListener("loadend", function() {
@@ -46,10 +51,7 @@
 
 
   function addContent(offset) {
-    if (currentOffset === null) return;
-
     return getContent({ offset: offset}, function(err, response) {
-
       if (err) {
         currentOffset = null;
         return false;
@@ -65,25 +67,21 @@
 
       updateTemplate(data);
 
-      currentOffset += 9;
-
       return true;
     });
   }
 
-  window.addEventListener('scroll', (event) => {
-    var d = document.documentElement;
-    var offset = d.scrollTop + window.innerHeight;
-    var height = d.offsetHeight;
+  function handleScroll(event) {
+    if (currentOffset === null) return;
 
-    const percentageScrolled = Math.round((offset / height) * 100);
+    var docElement = document.documentElement;
+    var offset = docElement.scrollTop + window.innerHeight;
+    var height = docElement.offsetHeight;
 
     if (offset === height) {
+      currentOffset += paginateOffset;
+
       addContent(currentOffset);
     }
-
-  });
-
-  // kick things off;
-  addContent(currentOffset);
+  }
 })();
