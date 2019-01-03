26e560d384f86e61b87d9a368d76153787d5570a
Steven Bapaga <sbapaga@gmail.com>
2019-01-03 09:26:49 +0000

    Comment out tags test

--- moj-node/test-e2e/pages/tags-test.spec.js
+++ moj-node/test-e2e/pages/tags-test.spec.js
@@ -1,24 +1,24 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
-const config = require('../config');
+// const config = require('../config');
 
-describe('Tags page', () => {
-  let browser;
-  let page;
+// describe('Tags page', () => {
+//   let browser;
+//   let page;
 
-  before(async () => {
-    browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    page = await browser.newPage();
-  });
+//   before(async () => {
+//     browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+//     page = await browser.newPage();
+//   });
 
-  after(async () => {
-    await browser.close();
-  });
+//   after(async () => {
+//     await browser.close();
+//   });
 
-  it('navigates to a tags page from content', () => {
-    // click a video radio show
-    // find and click on a tag for it
-    // wait for the page to navigate
-    // verify we are on the correct page
-  });
-});
+//   it('navigates to a tags page from content', () => {
+//     // click a video radio show
+//     // find and click on a tag for it
+//     // wait for the page to navigate
+//     // verify we are on the correct page
+//   });
+// });
