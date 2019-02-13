cd4774dfd5ac20dd7f86056fd3863f65c3d661ed
Steven Bapaga <sbapaga@gmail.com>
2019-02-13 14:27:17 +0000

    Fix element.matches polyfill

--- moj-node/assets/javascript/polyfills.js
+++ moj-node/assets/javascript/polyfills.js
@@ -1,6 +1,19 @@
 // IE Polyfill
+
+// Element.matchs polyfill
 if (!Element.prototype.matches) {
-  Element.prototype.matches = Element.prototype.msMatchesSelector;
+  Element.prototype.matches =
+    Element.prototype.matchesSelector ||
+    Element.prototype.mozMatchesSelector ||
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.oMatchesSelector ||
+    Element.prototype.webkitMatchesSelector ||
+    function(s) {
+      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+        i = matches.length;
+      while (--i >= 0 && matches.item(i) !== this) {}
+      return i > -1;
+    };
 }
 
 // Object.assign polyfill
