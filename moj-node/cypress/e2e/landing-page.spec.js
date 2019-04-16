9af29b62f2e82aee287bcb1ea3ca951a945ac834
Lucas Cairns <lucas.cairns@gmail.com>
2019-04-16 16:34:59 +0100

    Add Berwyn Life link to navigation

--- moj-node/cypress/e2e/landing-page.spec.js
+++ moj-node/cypress/e2e/landing-page.spec.js
@@ -16,12 +16,10 @@ describe('Landing page', () => {
       cy.visit(landingPages[key]);
 
       cy.log('Should have some navigation menu items');
-      cy.get('.govuk-hub-content-section-menu li').then($el => {
-        expect($el.length).to.be.at.least(
-          2,
-          'Should have at least 2 menu items',
-        );
-      });
+      cy.get('.govuk-hub-content-section-menu li').should(
+        'have.length.of.at.least',
+        2,
+      );
 
       cy.log('Check that featured content is rendered on the page');
       cy.get('[data-featured-id]').then($el => {
