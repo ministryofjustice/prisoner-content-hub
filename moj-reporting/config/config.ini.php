ad2fcf2e7e85279e1442d53ecff5c3b678e63076
James Stanley <James Stanley>
2019-05-10 11:24:48 +0100

    added secret and altered env variables

---
+++ moj-reporting/config/config.ini.php
@@ -0,0 +1,13 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+; file automatically generated or modified by Matomo; you can manually override the default values in global.ini.php by redefining them in this file.
+[database]
+host = "hub-matomo-db"
+username = "matomo_user"
+password = "matomo_pass"
+dbname = "matomo"
+tables_prefix = "matomo_"
+
+[General]
+salt = "5304d5ea7acc3ea0210a1a14cfd15064"
+installation_in_progress = 1
+
