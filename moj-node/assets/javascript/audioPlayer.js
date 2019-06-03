1341f1bbe2603a76bcc53f43971ffde3f52f9606
Steven Bapaga <sbapaga@gmail.com>
2019-06-03 10:13:24 +0100

    Update audioPlayer script

--- moj-node/assets/javascript/audioPlayer.js
+++ moj-node/assets/javascript/audioPlayer.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
   var audio = $('#audioPlayer');
   var programmeCode = audio.data().programmeCode;
-  var poster = audio.data().poster;
   var title = audio.data().title;
-  var height = audio.data().height;
-  var width = audio.data().width;
-  var media = audio.data().media;
+  var extendConfig = audio.data().config || {};
   var name = title + '|' + programmeCode;
 
   var evt25 = once(matomoAudioEvent({ action: '25%', name: name }));
@@ -13,16 +10,16 @@ $(document).ready(function() {
   var evt75 = once(matomoAudioEvent({ action: '75%', name: name }));
   var evt90 = once(matomoAudioEvent({ action: '90%', name: name }));
 
-  audio.videoPlayer({
+  var config = {
     name: title,
     size: {
-      width: width || '100%',
-      height: height || 'auto',
+      width: '100%',
+      height: 'auto',
     },
     media: {
       preload: 'metadata',
-      mp3: media,
-      poster: poster,
+      mp3: audio.data().media,
+      poster: audio.data().poster,
     },
     timeupdate: function(event) {
       var percentage = Math.round(event.jPlayer.status.currentPercentAbsolute);
@@ -36,7 +33,9 @@ $(document).ready(function() {
         evt90();
       }
     },
-  });
+  };
+
+  audio.videoPlayer(Object.assign(config, extendConfig));
 
   function matomoAudioEvent(config) {
     return function() {
