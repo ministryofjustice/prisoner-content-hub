ab87f2c97ff6851db1eec0cd26371f376c6d3120
Lucas Cairns <lucas.cairns@gmail.com>
2019-07-17 10:27:18 +0100

    Update typeahead suggestion template
    Search suggestions are now presented as direct links to content

--- moj-node/assets/javascript/search.js
+++ moj-node/assets/javascript/search.js
@@ -1,21 +1,41 @@
-var search = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  remote: {
-    url: '/search/suggest/%QUERY',
-    wildcard: '%QUERY',
-  },
-});
+(function() {
+  var search = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: '/search/suggest/%QUERY',
+      wildcard: '%QUERY',
+    },
+  });
 
-$('#search-wrapper .search-box').typeahead(
-  {
-    hint: true,
-    highlight: true,
-    minLength: 3,
-  },
-  {
-    name: 'search-results',
-    display: 'title',
-    source: search,
-  },
-);
+  $('#search-wrapper .search-box').typeahead(
+    {
+      hint: true,
+      highlight: true,
+      minLength: 3,
+    },
+    {
+      name: 'search-results',
+      display: 'title',
+      source: search,
+      templates: {
+        suggestion: createSuggestionList,
+      },
+    },
+  );
+
+  function createSuggestionList(value) {
+    var query = value._query.replace(/\s+/, ' ');
+    var title = value.title;
+    var reg = new RegExp(query, 'gi');
+    var url = value.url || '#';
+    var label = title.replace(reg, function(match) {
+      return '<strong>' + match + '</strong>';
+    });
+
+    // prettier-ignore
+    return (
+      '<a role="option" class="govuk-link  govuk-link--no-visited-state tt-suggestion tt-selectable" href="' + url + '">' + label + '</a>'
+    );
+  }
+})();
