72769b7dcd6f5e9f0d658c59c3bfa4d88355631d
David Thomas <david.thomas1@digital.justice.gov.uk>
2019-07-17 11:21:43 +0100

    Move script tags to bodyEnd block
    - Turn all scripts to IIFE to prevent global leaks

--- moj-node/assets/javascript/all.js
+++ moj-node/assets/javascript/all.js
@@ -1,42 +1,44 @@
-document.body.addEventListener('click', event => {
-  if (matchesAll('[data-state]', event.target)) {
-    var element = getAncestorElementBySelector('[data-state]', event.target);
+(function() {
+  document.body.addEventListener('click', event => {
+    if (matchesAll('[data-state]', event.target)) {
+      var element = getAncestorElementBySelector('[data-state]', event.target);
 
-    if (element) {
-      var state = element.getAttribute('data-state');
-      var stateToggle = function() {
-        element.setAttribute('data-state', 'hidden');
-      };
-      element.setAttribute('data-state', toggleShowing(state));
+      if (element) {
+        var state = element.getAttribute('data-state');
+        var stateToggle = function() {
+          element.setAttribute('data-state', 'hidden');
+        };
+        element.setAttribute('data-state', toggleShowing(state));
 
-      if (state == 'hidden') {
-        clearTimeout(stateToggle);
+        if (state == 'hidden') {
+          clearTimeout(stateToggle);
+        }
+        setTimeout(stateToggle, 5000);
       }
-      setTimeout(stateToggle, 5000);
     }
-  }
-});
+  });
 
-function toggleShowing(state) {
-  if (state === 'hidden') {
-    return 'showing';
-  }
+  function toggleShowing(state) {
+    if (state === 'hidden') {
+      return 'showing';
+    }
 
-  return 'hidden';
-}
-
-function matchesAll(selector, element) {
-  return element.matches(selector) || element.matches(selector + ' *');
-}
-
-function getAncestorElementBySelector(selector, element) {
-  if (element.matches(selector)) {
-    return element;
+    return 'hidden';
   }
 
-  if (element.parentNode) {
-    return getAncestorElementBySelector(selector, element.parentNode);
+  function matchesAll(selector, element) {
+    return element.matches(selector) || element.matches(selector + ' *');
   }
 
-  return null;
-}
+  function getAncestorElementBySelector(selector, element) {
+    if (element.matches(selector)) {
+      return element;
+    }
+
+    if (element.parentNode) {
+      return getAncestorElementBySelector(selector, element.parentNode);
+    }
+
+    return null;
+  }
+})();
--- moj-node/assets/javascript/hub.js
+++ moj-node/assets/javascript/hub.js
@@ -1,99 +1,100 @@
 /* eslint-env browser */
+(function() {
+  var navMenu = document.getElementById('nav-menu');
+  var todaysDate = document.querySelector('[data-target-id="todays-date"]');
+  var todaysTime = document.querySelector('[data-target-id="todays-time"]');
+  var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
 
-var navMenu = document.getElementById('nav-menu');
-var todaysDate = document.querySelector('[data-target-id="todays-date"]');
-var todaysTime = document.querySelector('[data-target-id="todays-time"]');
-var months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
+  var days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-var days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
+  // Navigation menu
+  if (navMenu) {
+    document.body.addEventListener('click', function(event) {
+      // handle navigation hide
+      if (matchesSelectorAll(event.target, '#nav-hide')) {
+        event.preventDefault();
+        navMenu.classList.remove('is-open');
+        return false;
+      }
 
-// Navigation menu
-if (navMenu) {
-  document.body.addEventListener('click', function(event) {
-    // handle navigation hide
-    if (matchesSelectorAll(event.target, '#nav-hide')) {
-      event.preventDefault();
-      navMenu.classList.remove('is-open');
+      // handle navigation menu btn
+      if (matchesSelectorAll(event.target, '#menu-btn')) {
+        event.preventDefault();
+        navMenu.classList.toggle('is-open');
+        return false;
+      }
+
+      // if nav is open ignore clicks to the navigation
+      if (matchesSelectorAll(event.target, '#nav-menu')) {
+        return false;
+      }
+
+      // close navigation menu if body is clicked and nav is open
+      if (navMenu.classList.contains('is-open')) {
+        return navMenu.classList.remove('is-open');
+      }
+
       return false;
-    }
+    });
+  }
 
-    // handle navigation menu btn
-    if (matchesSelectorAll(event.target, '#menu-btn')) {
-      event.preventDefault();
-      navMenu.classList.toggle('is-open');
-      return false;
-    }
+  // update the time
+  if (todaysTime) {
+    setInterval(function() {
+      var date = new Date();
+      var currentTime = todaysTime.textContent.trim();
+      var minutes = date.getMinutes();
+      var hours = date.getHours();
+      var nextTime = formateTime(hours) + ':' + formateTime(minutes);
+      if (currentTime !== nextTime) {
+        todaysTime.textContent = nextTime;
+      }
+    }, 1000);
+  }
 
-    // if nav is open ignore clicks to the navigation
-    if (matchesSelectorAll(event.target, '#nav-menu')) {
-      return false;
-    }
+  // update the date
+  if (todaysDate) {
+    setInterval(function() {
+      var date = new Date();
+      var currentDate = todaysDate.textContent.trim();
+      var nextDate =
+        days[date.getDay()] +
+        ' ' +
+        date.getDate() +
+        ' ' +
+        months[date.getMonth()];
+      if (currentDate !== nextDate) {
+        todaysDate.textContent = nextDate;
+      }
+    }, 1000);
+  }
 
-    // close navigation menu if body is clicked and nav is open
-    if (navMenu.classList.contains('is-open')) {
-      return navMenu.classList.remove('is-open');
-    }
+  function matchesSelectorAll(target, selector) {
+    return target.matches(selector) || target.matches(selector + ' *');
+  }
 
-    return false;
-  });
-}
-
-// update the time
-if (todaysTime) {
-  setInterval(function() {
-    var date = new Date();
-    var currentTime = todaysTime.textContent.trim();
-    var minutes = date.getMinutes();
-    var hours = date.getHours();
-    var nextTime = formateTime(hours) + ':' + formateTime(minutes);
-    if (currentTime !== nextTime) {
-      todaysTime.textContent = nextTime;
-    }
-  }, 1000);
-}
-
-// update the date
-if (todaysDate) {
-  setInterval(function() {
-    var date = new Date();
-    var currentDate = todaysDate.textContent.trim();
-    var nextDate =
-      days[date.getDay()] +
-      ' ' +
-      date.getDate() +
-      ' ' +
-      months[date.getMonth()];
-    if (currentDate !== nextDate) {
-      todaysDate.textContent = nextDate;
-    }
-  }, 1000);
-}
-
-function matchesSelectorAll(target, selector) {
-  return target.matches(selector) || target.matches(selector + ' *');
-}
-
-function formateTime(time) {
-  return time < 10 ? '0' + time : time;
-}
+  function formateTime(time) {
+    return time < 10 ? '0' + time : time;
+  }
+})();
