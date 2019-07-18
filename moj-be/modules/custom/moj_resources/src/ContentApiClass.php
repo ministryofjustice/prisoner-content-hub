616d3a400c72b17cb944b8cc3d10a7ee07407b63
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2019-07-18 15:44:20 +0100

    Show secondary tags rather than primary
    Currently the tag links are showing the primnary tags, make them show the secondary.
    
    - correct item response for secondary tags
    - correct decorate series for secondary tags
    - update mediaResponseFrom method for secondary tags

--- moj-be/modules/custom/moj_resources/src/ContentApiClass.php
+++ moj-be/modules/custom/moj_resources/src/ContentApiClass.php
@@ -134,7 +134,7 @@ class ContentApiClass
     $result["image"] =  $node->field_moj_thumbnail_image[0];
     $result["description"] =  $node->field_moj_description[0];
     $result["categories"] =  $node->field_moj_top_level_categories;
-    $result["secondary_tags"] =  $node->field_moj_tags;
+    $result["secondary_tags"] =  $node->field_moj_secondary_tags;
     $result["prisons"] =  $node->field_moj_prisons;
 
     return  $result;
