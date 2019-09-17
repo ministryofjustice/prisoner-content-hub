b2a9ce537eb1c55641dfe32aabb7813da577450a
Lucas Cairns <lucas.cairns@gmail.com>
2019-09-17 13:54:55 +0100

    Refactor personal information components
    * Split template out in to seperate components
    * Update offender service to handle errors
    * Update components to support fallback behaviours

--- moj-node/server/services/offender.js
+++ moj-node/server/services/offender.js
@@ -61,7 +61,6 @@ const getEventsForTimeOfDay = (events, timeOfDay) => {
 module.exports = function createOffenderService(repository) {
   async function getOffenderDetailsFor(prisonerId) {
     const response = await repository.getOffenderDetailsFor(prisonerId);
-
     const { bookingId, offenderNo, firstName, lastName } = response;
 
     return {
@@ -72,87 +71,118 @@ module.exports = function createOffenderService(repository) {
   }
 
   async function getIEPSummaryFor(bookingId) {
-    const iePSummary = await repository.getIEPSummaryFor(bookingId);
+    try {
+      const iePSummary = await repository.getIEPSummaryFor(bookingId);
 
-    return {
-      reviewDate: 'Unavailable',
-      iepLevel: iePSummary.iepLevel,
-      daysSinceReview: distanceInWords(parse(iePSummary.iepDate), new Date()),
-    };
+      return {
+        reviewDate: 'Unavailable',
+        iepLevel: iePSummary.iepLevel,
+        daysSinceReview: distanceInWords(parse(iePSummary.iepDate), new Date()),
+      };
+    } catch {
+      return {
+        error: 'We are not able to show your IEP summary at this time',
+      };
+    }
   }
 
   async function getBalancesFor(bookingId) {
-    const balances = await repository.getBalancesFor(bookingId);
-    const getOrDefault = propOr('Unavailable');
+    try {
+      const balances = await repository.getBalancesFor(bookingId);
+      const getOrDefault = propOr('Unavailable');
 
-    return {
-      spends: getOrDefault('spends', balances),
-      cash: getOrDefault('cash', balances),
-      savings: getOrDefault('savings', balances),
-      currency: getOrDefault('currency', balances),
-    };
+      return {
+        spends: getOrDefault('spends', balances),
+        cash: getOrDefault('cash', balances),
+        savings: getOrDefault('savings', balances),
+        currency: getOrDefault('currency', balances),
+      };
+    } catch {
+      return {
+        error: 'We are not able to show your balances at this time',
+      };
+    }
   }
 
   async function getKeyWorkerFor(prisonerId) {
-    const keyWorker = await repository.getKeyWorkerFor(prisonerId);
+    try {
+      const { firstName, lastName } = await repository.getKeyWorkerFor(
+        prisonerId,
+      );
 
-    if (!keyWorker) {
       return {
-        current: 'Unavailable',
+        current: `${capitalize(firstName)} ${capitalize(lastName)}`,
         lastMeeting: 'Unavailable',
       };
+    } catch {
+      return {
+        error: 'We are not able to show Key Worker information at this time',
+      };
     }
-
-    return {
-      current: `${capitalize(keyWorker.firstName)} ${capitalize(
-        keyWorker.lastName,
-      )}`,
-      lastMeeting: 'Unavailable',
-    };
   }
 
   async function getVisitsFor(bookingId) {
-    const lastVisit = await repository.getLastVisitFor(bookingId);
-    const nextVisit = await repository.getNextVisitFor(bookingId);
+    try {
+      const lastVisit = await repository.getLastVisitFor(bookingId);
+      const nextVisit = await repository.getNextVisitFor(bookingId);
 
-    return {
-      lastVisit: prettyDate(prop('startTime', lastVisit)),
-      nextVisit: prettyDate(prop('startTime', nextVisit)),
-    };
+      return {
+        lastVisit: prettyDate(prop('startTime', lastVisit)),
+        nextVisit: prettyDate(prop('startTime', nextVisit)),
+      };
+    } catch {
+      return {
+        error: 'We are not able to show your visits at this time',
+      };
+    }
   }
 
   async function getImportantDatesFor(bookingId) {
-    const sentenceDetails = await repository.sentenceDetailsFor(bookingId);
+    try {
+      const sentenceDetails = await repository.sentenceDetailsFor(bookingId);
 
-    return {
-      reCategorisationDate: 'Unavailable',
-      hdcEligibilityDate: prettyDate(
-        prop('homeDetentionCurfewEligibilityDate', sentenceDetails),
-      ),
-      conditionalReleaseDate: prettyDate(
-        prop('conditionalReleaseDate', sentenceDetails),
-      ),
-      licenceExpiryDate: prettyDate(prop('licenceExpiryDate', sentenceDetails)),
-    };
+      return {
+        reCategorisationDate: 'Unavailable',
+        hdcEligibilityDate: prettyDate(
+          prop('homeDetentionCurfewEligibilityDate', sentenceDetails),
+        ),
+        conditionalReleaseDate: prettyDate(
+          prop('conditionalReleaseDate', sentenceDetails),
+        ),
+        licenceExpiryDate: prettyDate(
+          prop('licenceExpiryDate', sentenceDetails),
+        ),
+      };
+    } catch {
+      return {
+        error: 'We are not able to show your important dates at this time',
+      };
+    }
   }
 
   async function getEventsForToday(bookingId) {
-    const noEvents = {
-      morning: [],
-      afternoon: [],
-      evening: [],
-    };
-    if (!bookingId) return noEvents;
+    try {
+      const noEvents = {
+        morning: [],
+        afternoon: [],
+        evening: [],
+      };
+      if (!bookingId) return noEvents;
 
-    const events = await repository.getEventsForToday(bookingId);
+      const events = await repository.getEventsForToday(bookingId);
 
-    if (!Array.isArray(events)) return noEvents;
+      if (!Array.isArray(events)) return noEvents;
 
-    return {
-      morning: getEventsForTimeOfDay(events, 'morning'),
-      afternoon: getEventsForTimeOfDay(events, 'afternoon'),
-      evening: getEventsForTimeOfDay(events, 'evening'),
-    };
+      return {
+        morning: getEventsForTimeOfDay(events, 'morning'),
+        afternoon: getEventsForTimeOfDay(events, 'afternoon'),
+        evening: getEventsForTimeOfDay(events, 'evening'),
+      };
+    } catch {
+      return {
+        error: 'We are not able to show your schedule for today at this time',
+      };
+    }
   }
 
   return {
