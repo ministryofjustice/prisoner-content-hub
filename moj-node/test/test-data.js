ab8c247db3c1fc5ce40ea480b5ab89e48b04ead1
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2019-10-31 13:24:00 +0000

    Use new events endpoint
    - Update endpoint url
    - Add test for new endpoint

--- moj-node/test/test-data.js
+++ moj-node/test/test-data.js
@@ -82,158 +82,323 @@ const getEventsForTodayData = [
 const getEventsForData = [
   {
     title: 'single appointment',
-    startDate: nowDateString,
-    endDate: nowDateString,
+    startDate: '2011-01-01',
+    endDate: '2011-01-02',
     repo: [
       {
         eventSourceDesc: 'Some title',
-        startTime: `${nowDateString}T11:30:30`,
-        endTime: `${nowDateString}T12:30:30`,
+        startTime: `2011-01-01T11:30:30`,
+        endTime: `2011-01-01T12:30:30`,
         eventLocation: 'Some location',
         eventType: 'APP',
+        paid: false,
+        eventStatus: true,
       },
     ],
     data: {
-      morning: [
-        {
-          title: 'Some title',
-          startTime: '11:30AM',
-          endTime: '12:30PM',
-          timeString: '11:30AM',
-          location: 'Some location',
-          eventType: 'APP',
+      '2011-01-01': {
+        morning: {
+          events: [
+            {
+              description: 'Some title',
+              startTime: '11:30AM',
+              endTime: '12:30PM',
+              timeString: '11:30AM - 12:30PM',
+              location: 'Some location',
+              eventType: 'APP',
+              finished: true,
+              paid: false,
+              status: true,
+            },
+          ],
+          finished: true,
         },
-      ],
-      afternoon: [],
-      evening: [],
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Saturday 1 January',
+      },
+      '2011-01-02': {
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        morning: {
+          events: [],
+          finished: true,
+        },
+        title: 'Sunday 2 January',
+      },
     },
   },
   {
     title: 'single appointment no end time',
-    startDate: nowDateString,
-    endDate: nowDateString,
+    startDate: '2011-01-01',
+    endDate: '2011-01-02',
     repo: [
       {
         eventSourceDesc: 'Some title',
-        startTime: `${nowDateString}T11:30:30`,
+        startTime: `2011-01-01T11:30:30`,
         endTime: '',
         eventLocation: 'Some location',
         eventType: 'APP',
+        paid: false,
+        eventStatus: true,
       },
     ],
     data: {
-      morning: [
-        {
-          title: 'Some title',
-          startTime: '11:30AM',
-          endTime: '',
-          timeString: '11:30AM',
-          location: 'Some location',
-          eventType: 'APP',
+      '2011-01-01': {
+        morning: {
+          events: [
+            {
+              description: 'Some title',
+              startTime: '11:30AM',
+              endTime: '',
+              timeString: '11:30AM',
+              location: 'Some location',
+              eventType: 'APP',
+              finished: true,
+              paid: false,
+              status: true,
+            },
+          ],
+          finished: true,
         },
-      ],
-      afternoon: [],
-      evening: [],
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Saturday 1 January',
+      },
+      '2011-01-02': {
+        morning: {
+          events: [],
+          finished: true,
+        },
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Sunday 2 January',
+      },
     },
   },
   {
     title: 'single visit',
-    startDate: nowDateString,
-    endDate: nowDateString,
+    startDate: '2011-01-01',
+    endDate: '2011-01-02',
     repo: [
       {
         eventSourceDesc: 'Some title',
-        startTime: `${nowDateString}T11:30:30`,
-        endTime: `${nowDateString}T12:30:30`,
+        startTime: `2011-01-01T11:30:30`,
+        endTime: `2011-01-01T12:30:30`,
         eventLocation: 'Some location',
         eventType: 'VISIT',
+        paid: false,
+        eventStatus: true,
       },
     ],
     data: {
-      morning: [
-        {
-          title: 'Some title',
-          startTime: '11:30AM',
-          endTime: '12:30PM',
-          timeString: '11:30AM',
-          location: 'Some location',
-          eventType: 'VISIT',
+      '2011-01-01': {
+        morning: {
+          events: [
+            {
+              description: 'Some title',
+              startTime: '11:30AM',
+              endTime: '12:30PM',
+              timeString: '11:30AM - 12:30PM',
+              location: 'Some location',
+              eventType: 'VISIT',
+              paid: false,
+              status: true,
+              finished: true,
+            },
+          ],
+          finished: true,
         },
-      ],
-      afternoon: [],
-      evening: [],
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Saturday 1 January',
+      },
+      '2011-01-02': {
+        morning: {
+          events: [],
+          finished: true,
+        },
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Sunday 2 January',
+      },
     },
   },
   {
     title: 'single activity',
-    startDate: nowDateString,
-    endDate: nowDateString,
+    startDate: '2011-01-01',
+    endDate: '2011-01-02',
     repo: [
       {
         eventSourceDesc: 'Some title',
-        startTime: `${nowDateString}T11:30:30`,
-        endTime: `${nowDateString}T12:30:30`,
+        startTime: `2011-01-01T11:30:30`,
+        endTime: `2011-01-01T12:30:30`,
         eventLocation: 'Some location',
         eventType: 'PRISON_ACT',
+        paid: false,
+        eventStatus: true,
       },
     ],
     data: {
-      morning: [
-        {
-          title: 'Some title',
-          startTime: '11:30AM',
-          endTime: '12:30PM',
-          timeString: '11:30AM',
-          location: 'Some location',
-          eventType: 'PRISON_ACT',
+      '2011-01-01': {
+        morning: {
+          events: [
+            {
+              description: 'Some title',
+              startTime: '11:30AM',
+              endTime: '12:30PM',
+              timeString: '11:30AM - 12:30PM',
+              location: 'Some location',
+              eventType: 'PRISON_ACT',
+              paid: false,
+              status: true,
+              finished: true,
+            },
+          ],
+          finished: true,
         },
-      ],
-      afternoon: [],
-      evening: [],
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Saturday 1 January',
+      },
+      '2011-01-02': {
+        morning: {
+          events: [],
+          finished: true,
+        },
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Sunday 2 January',
+      },
     },
   },
   {
     title: 'multiple activities',
-    startDate: nowDateString,
-    endDate: nowDateString,
+    startDate: '2011-01-01',
+    endDate: '2011-01-02',
     repo: [
       {
         eventSourceDesc: 'Some title 1',
-        startTime: `${nowDateString}T11:30:30`,
-        endTime: `${nowDateString}T12:30:30`,
+        startTime: `2011-01-01T11:30:30`,
+        endTime: `2011-01-01T12:30:30`,
         eventLocation: 'Some location 1',
         eventType: 'PRISON_ACT',
+        paid: false,
+        eventStatus: true,
       },
       {
         eventSourceDesc: 'Some title 2',
-        startTime: `${nowDateString}T12:30:30`,
-        endTime: `${nowDateString}T13:30:30`,
+        startTime: `2011-01-01T12:30:30`,
+        endTime: `2011-01-01T13:30:30`,
         eventLocation: 'Some location 2',
         eventType: 'VISIT',
+        paid: false,
+        eventStatus: true,
       },
     ],
     data: {
-      morning: [
-        {
-          title: 'Some title 1',
-          startTime: '11:30AM',
-          endTime: '12:30PM',
-          timeString: '11:30AM',
-          location: 'Some location 1',
-          eventType: 'PRISON_ACT',
+      '2011-01-01': {
+        morning: {
+          events: [
+            {
+              description: 'Some title 1',
+              startTime: '11:30AM',
+              endTime: '12:30PM',
+              timeString: '11:30AM - 12:30PM',
+              location: 'Some location 1',
+              eventType: 'PRISON_ACT',
+              paid: false,
+              status: true,
+              finished: true,
+            },
+          ],
+          finished: true,
         },
-      ],
-      afternoon: [
-        {
-          title: 'Some title 2',
-          startTime: '12:30PM',
-          endTime: '1:30PM',
-          timeString: '12:30PM',
-          location: 'Some location 2',
-          eventType: 'VISIT',
+        afternoon: {
+          events: [
+            {
+              description: 'Some title 2',
+              startTime: '12:30PM',
+              endTime: '1:30PM',
+              timeString: '12:30PM - 1:30PM',
+              location: 'Some location 2',
+              eventType: 'VISIT',
+              paid: false,
+              status: true,
+              finished: true,
+            },
+          ],
+          finished: true,
         },
-      ],
-      evening: [],
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Saturday 1 January',
+      },
+      '2011-01-02': {
+        morning: {
+          events: [],
+          finished: true,
+        },
+        afternoon: {
+          events: [],
+          finished: true,
+        },
+        evening: {
+          events: [],
+          finished: true,
+        },
+        title: 'Sunday 2 January',
+      },
     },
   },
 ];
