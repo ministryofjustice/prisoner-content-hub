47bdf8154fefcd254de5cd8ea922de7d1af6458d
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2019-12-12 10:57:11 +0000

    Add link to IEP

--- moj-node/server/routes/home.js
+++ moj-node/server/routes/home.js
@@ -48,7 +48,7 @@ module.exports = function Home({
 
       const popularTopics = {
         Visits: '/content/4203',
-        Incentives: '/content/4204',
+        Incentives: '/iep',
         Games: '/content/3621',
         Inspiration: '/content/3659',
         'Music & talk': '/content/3662',
