d2bde605a462fe4be00b15bee6162ec36dfbe7dd
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-01-10 11:24:21 +0000

    Capitalize visitor name
    - Capitalize each word
    - Also deal with double barrelled surname

--- moj-node/test/utils/index.spec.js
+++ moj-node/test/utils/index.spec.js
@@ -1,6 +1,131 @@
-const { relativeUrlFrom } = require('../../server/utils/index');
+const {
+  relativeUrlFrom,
+  capitalize,
+  capitalizeAll,
+  capitalizePersonName,
+} = require('../../server/utils/index');
 
 describe('Utils', () => {
+  describe('capitalize', () => {
+    [
+      {
+        input: 'testword',
+        output: 'Testword',
+      },
+      {
+        input: '',
+        output: '',
+      },
+      {
+        input: 'TESTWORD',
+        output: 'Testword',
+      },
+      {
+        input: 'Testword',
+        output: 'Testword',
+      },
+      {
+        input: 'teSTword',
+        output: 'Testword',
+      },
+      {
+        input: 'teSTword testy',
+        output: 'Testword testy',
+      },
+    ].forEach(test => {
+      it(`should capitalize "${test.input}" to "${test.output}"`, () => {
+        expect(capitalize(test.input)).to.equal(test.output);
+      });
+    });
+  });
+  describe('capitalizeAll', () => {
+    [
+      {
+        input: 'testword',
+        output: 'Testword',
+      },
+      {
+        input: '',
+        output: '',
+      },
+      {
+        input: 'TESTWORD',
+        output: 'Testword',
+      },
+      {
+        input: 'Testword',
+        output: 'Testword',
+      },
+      {
+        input: 'teSTword',
+        output: 'Testword',
+      },
+      {
+        input: 'teSTword testy',
+        output: 'Testword Testy',
+      },
+      {
+        input: 'teSTword tEsty',
+        output: 'Testword Testy',
+      },
+      {
+        input: 'teSTword-testy',
+        output: 'Testword-testy',
+      },
+    ].forEach(test => {
+      it(`should capitalize "${test.input}" to "${test.output}"`, () => {
+        expect(capitalizeAll(test.input)).to.equal(test.output);
+      });
+    });
+  });
+  describe('capitalizePersonName', () => {
+    [
+      {
+        input: 'testword',
+        output: 'Testword',
+      },
+      {
+        input: '',
+        output: '',
+      },
+      {
+        input: 'TESTWORD',
+        output: 'Testword',
+      },
+      {
+        input: 'Testword',
+        output: 'Testword',
+      },
+      {
+        input: 'teSTword',
+        output: 'Testword',
+      },
+      {
+        input: 'teSTword testy',
+        output: 'Testword Testy',
+      },
+      {
+        input: 'teSTword tEsty',
+        output: 'Testword Testy',
+      },
+      {
+        input: 'teSTword-testy',
+        output: 'Testword-Testy',
+      },
+      {
+        input: 'teSTword tEsty-berp',
+        output: 'Testword Testy-Berp',
+      },
+      {
+        input: 'teSTword testyMc-Testtes',
+        output: 'Testword Testymc-Testtes',
+      },
+    ].forEach(test => {
+      it(`should capitalize "${test.input}" to "${test.output}"`, () => {
+        expect(capitalizePersonName(test.input)).to.equal(test.output);
+      });
+    });
+  });
   describe('relativeUrlFrom', () => {
     it('should remove the scheme and host from the url', () => {
       const result = relativeUrlFrom('https://foo.bar.com/some/relative/path');
