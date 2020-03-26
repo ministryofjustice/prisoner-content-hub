cf7bcb08f49b31361f7d7167cbce1a48aee0ee40
David Thomas <david.thomas1@digital.justice.gov.uk>
2020-03-26 13:48:33 +0100

    Add win lose functionality *Create seperate win / lose function *Add win / lose canvas message *Add restart game functionality

--- moj-node/assets/javascript/games/breakout/index.js
+++ moj-node/assets/javascript/games/breakout/index.js
@@ -1,33 +1,35 @@
 var gameCanvas = document.getElementById("gameCanvas");
-    var ctx = gameCanvas.getContext("2d");
-    var x = gameCanvas.width/2;
-    var y = gameCanvas.height-30;
-    var dx = 5;
-    var dy = -5 ;
-    var ballRadius = 10;
-    var randomColor = getRandomColor();
-    var blockColor = ["#d4351c", 	"#ffdd00", "#00703c", "#1d70b8", "#003078",  "#5694ca", "#4c2c92"];
-    var paddleHeight = 10;
-    var paddleWidth = 100;
-    var paddleX = (gameCanvas.width-paddleWidth) / 2;
-    var rightPressed = false;
-    var leftPressed = false;
-    var brickRowCount = 3;
-    var brickColumnCount = 5;
-    var brickWidth = 100;
-    var brickHeight = 40;
-    var brickPadding = 20;
-    var brickOffsetTop = 60;
-    var brickOffsetLeft = 250;
-    var score = 0;
-    var lives = 3;
-    var bricks = [];
-    for(var c=0; c<brickColumnCount; c++) {
-        bricks[c] = [];
-        for(var r=0; r<brickRowCount; r++) {
-            bricks[c][r] = { x: 0, y: 0, status: 1 };
-        }
+var ctx = gameCanvas.getContext("2d");
+var x = gameCanvas.width/2;
+var y = gameCanvas.height-30;
+var dx = 5;
+var dy = -5;
+var ballStatic = false;
+var ballRadius = 10;
+var randomColor = getRandomColor();
+var blockColor = ["#d4351c", 	"#ffdd00", "#00703c", "#1d70b8", "#003078",  "#5694ca", "#4c2c92"];
+var paddleHeight = 10;
+var paddleWidth = 100;
+var paddleX = (gameCanvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+var spaceBarPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 40;
+var brickHeight = 40;
+var brickPadding = 50;
+var brickOffsetTop = 60;
+var brickOffsetLeft = 150;
+var score = 0;
+var lives = 3;
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
+}
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
@@ -64,7 +66,7 @@ function drawBricks() {
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = blockColor[c];
+                ctx.fillStyle = getRandomColor();
                 ctx.fill();
                 ctx.closePath();
             }
@@ -81,6 +83,11 @@ function draw() {
     drawLives();
     collisionDetection()
 
+    if(score == brickRowCount*brickColumnCount) {
+      winGame();
+      return
+    }
+
     if(x + dx > gameCanvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
         randomColor = getRandomColor();
@@ -91,50 +98,53 @@ function draw() {
         randomColor = getRandomColor();
     }
 
-    else if(y + dy > gameCanvas.height-ballRadius) {
-        if(x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy;
-            }
+    else if (y + dy > gameCanvas.height - ballRadius - paddleHeight){
+      if(x > paddleX - ballRadius && x < paddleX + paddleWidth + ballRadius){
+          dy = -dy;
+      }
+      else {
+          lives --;
+          if(!lives){
+            loseGame()
+            return
+          }
 
-    else {
-    lives--;
-        if (lives === 3) {
-            requestAnimationFrame();
+          else {
+            x = paddleX + (paddleWidth / 2);
+            y = gameCanvas.height - paddleHeight - ballRadius - 0;
+            dx = 0;
+            dy = 0;
+            ballStatic = true;
+          }
         }
+      }
 
-        if(!lives) {
-            alert("GAME OVER you scored " + score + " points" );
-            document.location.reload();
-            requestAnimationFrame();
-            }
-        else {
-            requestAnimationFrame(function(){ alert("You lost a life"); }, 10);
-            x = gameCanvas.width/2;
-            y = gameCanvas.height-30;
-            dx = 5;
-            dy = -5;
-            paddleX = (gameCanvas.width-paddleWidth)/2;
-            rightPressed = false;
-            leftPressed = false;
-            }
-        }
-    }
-
-    if(rightPressed) {
+    if(rightPressed && ballStatic) {
         paddleX += 7;
+        x = paddleX + (paddleWidth / 2);
+        }
+
+    if(rightPressed && !ballStatic) {
+        paddleX += 7;
+        }
 
     if (paddleX + paddleWidth > gameCanvas.width){
         paddleX = gameCanvas.width - paddleWidth;
         }
-    }
 
-    else if(leftPressed) {
-        paddleX -= 7 ;
+
+    else if(leftPressed && ballStatic) {
+        paddleX -= 7;
+        x = paddleX + (paddleWidth / 2);
+        }
+
+    if(leftPressed && !ballStatic) {
+        paddleX -= 7;
+        }
 
     if (paddleX < 0){
         paddleX = 0;
         }
-    }
 
     x += dx;
     y += dy;
@@ -149,15 +159,26 @@ function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
     }
+
     else if(e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = true;
     }
+
+    else if(e.key == " " && ballStatic == true) {
+      dx = -5;
+      dy = 5;
+      ballStatic = false;
+    }
+
+    e.preventDefault();
+
 }
 
 function keyUpHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = false;
     }
+
     else if(e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
     }
@@ -172,10 +193,6 @@ function collisionDetection() {
                     dy = -dy;
                     b.status = 0;
                     score ++;
-                    if(score == brickRowCount*brickColumnCount) {
-                        alert("YOU WIN, CONGRATULATIONS!");
-                        document.location.reload();
-                    }
                 }
             }
         }
@@ -192,6 +209,39 @@ function drawLives() {
     ctx.font = "bold 18px Arial";
     ctx.fillStyle = "	#0b0c0c";
     ctx.fillText("Lives: "+lives, gameCanvas.width-80, 20);
+}
+
+function loseGame() {
+  ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.fillStyle = "#1d70b8";
+  ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.fillStyle = "white";
+  ctx.font = "bold 40px Arial";
+  ctx.fillText("Game over you scored " + score + " points" , 100 , gameCanvas.height / 2);
+  ctx.fillText("Press return to restart the game" , 100 , 380);
+
+  document.addEventListener('keyup', function(e){
+    if(e.keyCode == 13)
+    document.location.reload();
+    requestAnimationFrame();
+  })
+}
+
+function winGame() {
+  ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.fillStyle = "#1d70b8";
+  ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.fillStyle = "white";
+  ctx.font = "bold 40px Arial";
+  ctx.fillText("Smashed it!!! you win!!" , 100 , gameCanvas.height / 2);
+  ctx.fillText("Press return to start a new game" , 100 , 380);
+
+  document.addEventListener('keyup', function(e){
+    if(e.keyCode == 13)
+    document.location.reload();
+    requestAnimationFrame();
+  })
+
 }
 
 draw();
