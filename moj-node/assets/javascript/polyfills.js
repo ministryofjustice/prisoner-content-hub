97757456172e54ffb275bf2c1c5b70ca4db9802e
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-03-31 11:40:10 +0100

    Make work in IE11

--- moj-node/assets/javascript/polyfills.js
+++ moj-node/assets/javascript/polyfills.js
@@ -16,6 +16,31 @@ if (!Element.prototype.matches) {
     };
 }
 
+
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+
+    if (search instanceof RegExp) {
+      throw TypeError('first argument must not be a RegExp');
+    }
+    if (start === undefined) { start = 0; }
+    return this.indexOf(search, start) !== -1;
+  };
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
 // Object.assign polyfill
 if (typeof Object.assign != 'function') {
   // Must be writable: true, enumerable: false, configurable: true
