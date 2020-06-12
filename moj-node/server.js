cad69f8e208b43074a359448fd0da28d0ba59892
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2020-06-12 12:23:23 +0100

    Remove moj-node as in separate repo (#717)
    - Moved moj-node to prisoner-content-hub-frontend repo
    - Removed circle config as no longer required

--- moj-node/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const app = require('./server/index');
-const { logger } = require('./server/utils/logger');
-
-app.listen(app.get('port'), () => {
-  logger.info(`Server listening on port ${app.get('port')}`);
-});
